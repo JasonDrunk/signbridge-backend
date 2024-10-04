126be47ec9b00fae99117f4ffc1f4af187a248a7
JacklinKho <jacklinkho2002@gmail.com>
2024-10-04 16:51:39 +0800

    Done edit homepage functionality

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-require('dotenv').config(); // Load environment variables from .env file
+require("dotenv").config(); // Load environment variables from .env file
 
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const { MongoClient } = require('mongodb');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { MongoClient } = require("mongodb");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT;
@@ -15,50 +15,56 @@ const client = new MongoClient(dbUrl);
 const database = client.db(dbName);
 
 // Middleware
-app.use(cors({
-  origin: `http://localhost:${process.env.FRONTEND_PORT}`,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
+app.use(
+    cors({
+        origin: `http://localhost:${process.env.FRONTEND_PORT}`,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials: true,
+    })
+);
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 // -------------------Newly Added Working According to Folder Structure----------------------------------------
-const userRoutes = require("./routes/UserRoutes")
+const userRoutes = require("./routes/UserRoutes");
 app.use(userRoutes);
 
-const feedbackRoutes = require("./routes/FeedbackRoutes")
+const feedbackRoutes = require("./routes/FeedbackRoutes");
 app.use(feedbackRoutes);
 
-const faqRoutes = require("./routes/FaqRoutes")
+const faqRoutes = require("./routes/FaqRoutes");
 app.use(faqRoutes);
 
-const categoryRoutes = require("./routes/CategoryRoutes")
+const categoryRoutes = require("./routes/CategoryRoutes");
 app.use(categoryRoutes);
 
-const categorySignRoutes = require("./routes/CategorySignRoutes")
+const categorySignRoutes = require("./routes/CategorySignRoutes");
 app.use(categorySignRoutes);
 
-const communicationRoutes = require("./routes/CommunicationRoutes")
+const communicationRoutes = require("./routes/CommunicationRoutes");
 app.use(communicationRoutes);
 
-const notificationRoutes = require("./routes/NotificationRoutes")
+const notificationRoutes = require("./routes/NotificationRoutes");
 app.use(notificationRoutes);
 
-const leaderboardRoutes = require("./routes/LeaderBoardRoutes")
+const leaderboardRoutes = require("./routes/LeaderBoardRoutes");
 app.use(leaderboardRoutes);
 
-const formRoutes = require("./routes/DatasetFormRoutes")
+const formRoutes = require("./routes/DatasetFormRoutes");
 app.use(formRoutes);
+
+const edithomepageRoutes = require("./routes/EditHomepageRoutes");
+app.use(edithomepageRoutes);
+
 // ------------------------------------------------------------------------------------------------------------
-const UserController = require("./controllers/UserController")
-const FaqController = require("./controllers/FaqController")
+const UserController = require("./controllers/UserController");
+const FaqController = require("./controllers/FaqController");
 const CategoryController = require("./controllers/CategoryController");
-const CategorySignController = require('./controllers/CategorySignController');
+const CategorySignController = require("./controllers/CategorySignController");
 app.listen(port, () => {
-  // UserController.insertPresetAccounts();
-  CategoryController.insertPresetSignCategories();
-  CategorySignController.updateAvatarSigns();
-  UserController.insertPresetCountry();
-  FaqController.insertFixFaq();
+    // UserController.insertPresetAccounts();
+    CategoryController.insertPresetSignCategories();
+    CategorySignController.updateAvatarSigns();
+    UserController.insertPresetCountry();
+    FaqController.insertFixFaq();
 });
